280ec5e011e568ee4f82e4d9e59a39811f3e4114
aayu04shi <aayushi636naik@gmail.com>
2026-04-23 00:57:58 +0530

    Simplify main.py by removing data cleaning and evaluation
    Removed data cleaning and evaluation steps, directly training the model with the dataset.

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-from src.data_preprocessing import load_data, clean_data
-from src.train_model import train_model
-from src.evaluate import evaluate
+import pandas as pd
+from src.train_model import train_model # type: ignore
 
 # Load dataset
 df = pd.read_csv("data/crime_dataset_india.csv")
 
-# Clean data
-df = clean_data(df)
-
-print(df.columns)
-
 # Train model
-model, X_test, y_test = train_model(df)
-
-# Evaluate model
-evaluate(model, X_test, y_test)
-
-accuracy = 0.89
-print(f"FINAL_ACCURACY:{accuracy}")
+train_model(df)
